94d3496a6e6332e166f2382d26c1c6446f113c09
Phạm Thanh Phong <phamthanhphong9u@gmail.com>
2026-05-15 18:13:56 +0700

    update api endpoint url

--- app/web/src/services/publicAxiosClient.js
+++ app/web/src/services/publicAxiosClient.js
@@ -3,7 +3,7 @@ import queryString from 'query-string';
 import { getTranslation } from '@/utils/translate';
 import { notifyError } from '@/utils/NotificationService';
 
-const SERVER_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+const SERVER_URL = import.meta.env.VITE_API_URL || 'https://backend.bkeuty.xyz';
 
 const publicAxiosClient = axios.create({
     baseURL: SERVER_URL,
